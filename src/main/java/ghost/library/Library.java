9df1d26d5e7d26c58ca17ae0305ebeb1f1ce0031
Ghost <duchojakub987@gmail.com>
2026-02-08 15:08:39 +0100

    blob

--- src/main/java/ghost/library/Library.java
+++ src/main/java/ghost/library/Library.java
@@ -1,13 +1,11 @@
 package ghost.library;
 
 
-import ghost.library.frontend.GUIManager;
-
 public class Library {
 
     public static void main(String[] args) {
 
-        GUIManager manager = new GUIManager();
+        Manager manager = new Manager();
         manager.run();
 
     }
---
+++ src/main/java/ghost/library/Manager.java
@@ -0,0 +1,11 @@
+package ghost.library;
+
+import ghost.library.frontend.MainPage;
+
+public final class Manager {
+    
+    public void run() {
+        new MainPage().createWindow();
+    }
+
+}
